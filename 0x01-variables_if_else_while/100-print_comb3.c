3b938a2c69531d723debe7c0be4be550605b7fa3
Damian Emmanuel <damiannkemdirim001@gmail.com>
2022-06-05 16:53:44 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 00 to 99
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b;
+for (a = 48; a <= 57; a++)
+{
+for (b = 48; b <= 57; b++)
+	{
+	putchar(a);
+	putchar(b);
+	if (a != 57 || b != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+}
+putchar('\n');
+return (0);
+}
