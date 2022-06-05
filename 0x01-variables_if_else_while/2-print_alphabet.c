655df5d675dbb709e11e0e69f0000f6c14d34e15
Damian Emmanuel <damiannkemdirim001@gmail.com>
2022-06-05 05:26:31 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - assign a random number to the variable n each time it is executed and prints out based a condition
+ * Return : Always 0 (Sucess)
+ */
+int main(void)
+{
+	int ch;
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}	
+	putchar(10); /* this is an ascii code for new line */
+
+	return (0);
+}	
