ed4bf6d5ca03edb9f52845b03133bea666ddb05f
Damian Emmanuel <damiannkemdirim001@gmail.com>
2022-06-05 17:23:40 -0700

    tasks 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,15 +10,15 @@ int a, b;
 for (a = 48; a <= 57; a++)
 {
 for (b = 48; b <= 57; b++)
-	{
-	putchar(a);
-	putchar(b);
-	if (a != 57 || b != 57)
-	{
-	putchar(',');
-	putchar(' ');
-	}
-	}
+{
+putchar(a);
+putchar(b);
+if (a != 57 || b != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
