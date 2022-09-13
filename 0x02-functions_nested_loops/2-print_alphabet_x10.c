979edeec83e22ce3c8e72ed63b099a3e52949a5b
Damian-Emmanuel <damianemmanuel82@gmail.com>
2022-09-13 13:23:21 -0700

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- *
- * print_alphabet_x10 - function to print abc 10 times
- *
- * Return: 0
+ * main - function to print abc 10 times
+ * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
