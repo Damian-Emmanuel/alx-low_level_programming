74f7109cd391b1b2d2a2d5219d9c09a66c5f81d3
Damian-Emmanuel <damianemmanuel82@gmail.com>
2022-10-13 13:23:00 -0700

    task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -2,8 +2,8 @@
 
 /**
  * int_index - searches for an integer
- * @array - array to search in
- * @size - size of the array
+ * @array: array to search in
+ * @size: size of the array
  * @cmp: pointer to the comparing function
  *
  * Return: index of the first element for which
@@ -18,7 +18,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i] != 0)
+			if (cmp(array[i]) != 0)
 				return (i);
 		}
 	}
