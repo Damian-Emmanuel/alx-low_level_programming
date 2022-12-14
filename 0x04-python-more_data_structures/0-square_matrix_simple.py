533540ff4f3a7d66dc1b92d176b945f19b6d7a1a
Damian-Emmanuel <72523026+Damian-Emmanuel@users.noreply.github.com>
2022-12-14 20:28:36 +0100

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    def square_cells(array):
-        result = []
-        for cell in array:
-            result.append(cell ** 2)
-        return result
-    return list(map(square_cells, matrix))
--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-def square_matrix_map(matrix=[]):
-    return list(map(lambda 1: list(map(lambda n: n * n, l)), matrix))
